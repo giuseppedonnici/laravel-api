e270930c0f052d7db5ae4cb8918e07211ec5c860
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-06-07 20:20:56 +0200

    Add: select to crate view

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -9,12 +9,23 @@
         @csrf
         <div class="mb-3">
             <label for="title" class="form-label">Titolo</label>
-            <input type="text" class="form-control" id="title" name="title" value="{{old('title')}}">
-          </div>
-          <div class="mb-3">
+            <input type="text" class="form-control" id="title" name="title" value="{{ old('title') }}">
+        </div>
+
+        <div class="mb-3">
+            <label for="type">Tipologia</label>
+            <select class="form-select" id="type" name="type_id">
+                <option selected value=""></option>
+                @foreach ($types as $type)
+                <option value="{{ $type->id }}">{{ $type->name }}</option>
+                @endforeach
+            </select>
+        </div>
+
+        <div class="mb-3">
             <label for="description" class="form-label">Descrizione</label>
             <textarea class="form-control" id="description" rows="3" name="description">{{ old('description') }}</textarea>
-          </div>
-          <button class="btn btn-primary" type="submit">Invia</button>
+        </div>
+        <button class="btn btn-primary" type="submit">Invia</button>
     </form>
 @endsection
