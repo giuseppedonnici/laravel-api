4c3a3f2a1617bd082d27927ce7485a275c216055
giuseppedonnici <giuseppe.donnici8@gmail.com>
2023-06-08 15:53:08 +0200

    Add: technologies on projects\show view

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -10,5 +10,14 @@
         @endif
         <span>{{ $project->slug }}</span>
     </div>
+    <div>
+        <h5>Technologies: </h5>
+        @forelse ($project->technologies as $technology)
+            <span>{{ $technology->name }} {{ $loop->last ? '' : ',' }}</span>
+        @empty
+            <span>Nessuna tecnologia presente</span>
+        @endforelse
+
+    </div>
     <p class="mt-4">{{ $project->description }}</p>
 @endsection
